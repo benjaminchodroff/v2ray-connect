3b7d4d4410254f79dd02b9d98df9449c356e3878
Benjamin Chodroff <benjamin.chodroff@gmail.com>
2020-02-05 22:24:48 +0800

    update reset script

--- v2ray/reset.sh
+++ v2ray/reset.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker stop v2ray
-docker stop docker-nginx
-docker rm v2ray
-docker rm docker-nginx
-rm key.pem cert.pem config.json clashx.yaml shadowrocket.json
+docker rm -f v2ray
+rm config.json 
+rm -rf letsencrypt
+
